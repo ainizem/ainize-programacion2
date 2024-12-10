8285b27bf2d32619bc700f4c3fd2e12c9e73558c
ainizem <ainizemesquitac@gmail.com>
2024-12-10 22:36:39 +0100

    Add files via upload

---
+++ 09formulario/src/alumno/Alumno.java
@@ -0,0 +1,45 @@
+package alumno;
+
+public class Alumno {
+	
+	String nombre;
+	String apellido;
+	String curso;
+	String grado;
+	String comentario;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public String getGrado() {
+		return grado;
+	}
+	public void setGrado(String grado) {
+		this.grado = grado;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	
+	
+
+}
---
+++ 09formulario/src/formulario/Main.java
@@ -0,0 +1,12 @@
+package formulario;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Formulario frame = new Formulario();
+		frame.setVisible(true);
+
+	}
+
+}
