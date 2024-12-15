bc36222f0c2191156b3f1b2901ef08ac7e7680cd
ainizem <ainizemesquitac@gmail.com>
2024-12-15 22:19:55 +0100

    ListadoEstudiante.java

--- 09ventana_colegio/src/ui/ListadoEstudiante.java
+++ 09ventana_colegio/src/ui/ListadoEstudiante.java
@@ -16,20 +16,38 @@ public class ListadoEstudiante extends JFrame{
 	private JTable table;
 
 	public ListadoEstudiante(ArrayList<Estudiante> estudiantes) {
+		 
+	setTitle("Listado de Estudiantes");
+        setBounds(100, 100, 800, 400);
 		
 		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null},
-			},
-			new String[] {
-				"Necesidades", "Fecha Nacimiento", "Email", "Curso", "Apellido", "Nombre", "DNI"
-			}
-		));
+		 DefaultTableModel tableModel = new DefaultTableModel(
+            new Object[][] {}, // Inicialmente vacío
+            new String[] { // Encabezados de las columnas
+                "DNI", "Nombre", "Apellidos", "Email", "Fecha Nacimiento", "Curso", "Necesidades"
+            }
+        );
+
+        // Llenar la tabla con los datos de los estudiantes
+        for (Estudiante estudiante : estudiantes) {
+            tableModel.addRow(new Object[] {
+                estudiante.getDni(),
+                estudiante.getNombre(),
+                estudiante.getApellidos(),
+                estudiante.getEmail(),
+                estudiante.getFechaNacimiento().toString(),
+                estudiante.getCurso(),
+                estudiante.getNecesidadesEspeciales()
+            });
+        }
+
+        // Asignar el modelo a la tabla
+        table.setModel(tableModel);
+
+        // Añadir la tabla a la ventana
+        getContentPane().add(table, BorderLayout.CENTER);
+    }
+}
 		table.getColumnModel().getColumn(0).setResizable(false);
 		getContentPane().add(table, BorderLayout.CENTER);
 
